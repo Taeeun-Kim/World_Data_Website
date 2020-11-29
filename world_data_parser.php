5f5df969f1426f2fcc18e9b204217a24f0016da5
Taeeun Kim <taeeun.kim@outlook.com>
2020-11-29 21:59:38 +0100

    Updated server

---
+++ world_data_parser.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="js/table.js"></script>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>WorldDataParser</title>
+</head>
+
+<body>
+
+    <?php
+    $row = 1;
+    if (($handle = fopen("world_data_v1.csv", "r")) !== FALSE) {
+        while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+            $num = count($data);
+            echo "<p> $num fields in line $row: <br /></p>\n";
+            $row++;
+            for ($c=0; $c < $num; $c++) {
+                echo $data[$c] . "<br />\n";
+            }
+        }
+        fclose($handle);
+    }
+    class WorldDataParser{
+        public function parseCSV(){
+
+        }
+    }
+
+
+    ?>
+
+</body>
+
+</html>
