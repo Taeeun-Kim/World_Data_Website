65675702982fd74c9e8de0ef3b7eec042c9a9e7d
Taeeun Kim <taeeun.kim@outlook.com>
2020-12-16 03:39:30 +0100

    Added app.js

---
+++ app.js
@@ -0,0 +1,33 @@
+// DO NOT CHANGE!
+//init app with express, util, body-parser, csv2json
+const express = require('express');
+const app = express();
+const sys = require('util');
+const path = require('path');
+const bodyParser = require('body-parser');
+const csvConverter = require("csvtojson").Converter;
+
+//register body-parser to handle json from res / req
+app.use( bodyParser.json() );
+
+//register public dir to serve static files (html, css, js)
+app.use( express.static( path.join(__dirname, "public") ) );
+
+// END DO NOT CHANGE!
+
+
+/**************************************************************************
+****************************** csv2json *********************************
+**************************************************************************/
+
+/**************************************************************************
+********************** handle HTTP METHODS ***********************
+**************************************************************************/
+
+
+// DO NOT CHANGE!
+// bind server localhost to port 3000
+const port = 3000;
+app.listen(port, () => {
+	console.log(`Example app listening at http://localhost:${port}`);
+});
